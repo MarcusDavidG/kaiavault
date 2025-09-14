a227cafd49449e731209c6864b84b475c7ea71d8
MarcusDavidG <davidmarcus827@gmail.com>
2025-09-14 08:02:47 +0100

    feat: integrate RainbowKit and configure Kaia Kairos Testnet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 
 import './index.css'
+import '@rainbow-me/rainbowkit/styles.css'
 import App from './App.tsx'
 import { config } from './wagmi.ts'
 
@@ -13,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   </StrictMode>,
--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,13 +1,32 @@
-import { createConfig, http } from 'wagmi'
-import { kaia } from 'wagmi/chains'
-import { injected } from 'wagmi/connectors'
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { defineChain } from 'viem';
 
-export const config = createConfig({
-  chains: [kaia],
-  connectors: [
-    injected(),
-  ],
-  transports: {
-    [kaia.id]: http(),
+const kaiaKairosTestnet = defineChain({
+  id: 1001,
+  name: 'Kaia Kairos Testnet',
+  network: 'kaia-kairos-testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Kaia',
+    symbol: 'KAIA',
   },
-})
+  rpcUrls: {
+    default: {
+      http: ['https://public-en-cypress.klaytn.net'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'KlaytnScope',
+      url: 'https://baobab.klaytnscope.com/',
+    },
+  },
+  testnet: true,
+});
+
+export const config = getDefaultConfig({
+  appName: 'Kaiavault',
+  projectId: 'BPLBigIilwcsv6bd5UJsXmyS9SeEDBH3EwkdYICO-kMAhP69wOHiM02UvW_CUOKJKX6kh0kLi6phu1QO-FtB7rA',
+  chains: [kaiaKairosTestnet],
+  ssr: true,
+});
